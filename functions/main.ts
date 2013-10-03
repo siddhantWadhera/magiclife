b79ec4f51f214338804c1c4a221074e939ddb0b3
unknown <siddhant.w@BG1WS0089.SONATA.LOCAL>
2013-10-03 23:46:42 +0530

    header4

--- functions/main.ts
+++ functions/main.ts
@@ -269,7 +269,7 @@
       not(/false/) {
         # Do nothing :: Use base tag value
         $base_found = "true "
-		log("-------------------------------------------------------------------------------------> $base_found   : " + $base_found + "-------------------- : " + $rewriter_url + "---------------- $source_host: " + $source_host)
+		
       }
       else() {
         $rewriter_url = $source_host
@@ -287,7 +287,7 @@
             else() {
               match($base_found) {
                 with(/false/) {
-					log("-------------------------------------------------------------------------------------> $base_found is matched with false : " + $rewriter_url + "------------------ : " + $slash_path)
+					
                   # path-relative URL: add the host and the path
                   prepend(concat("//", $rewriter_url, $slash_path))
                 }
